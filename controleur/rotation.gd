b804c9bd06e49c594a69b1a3b2f7c994ba4508c7
TR <tomriesepro@gmail.com>
2024-11-27 10:24:52 +0100

    rotation faite sur le personnage et le monde

--- controleur/rotation.gd
+++ controleur/rotation.gd
@@ -10,24 +10,13 @@ func _input(event):
 		rotate_map(ROTATION_ANGLE)
 
 func rotate_map(angle_degrees):
-	print("Rotation demandée :", angle_degrees)
-	# Applique la rotation à Level_1
+	# Applique la rotation
 	rotation_degrees += angle_degrees
-	print("Nouvelle rotation :", rotation_degrees)
-	
-	# Verrouille la rotation sur des multiples de 90° pour éviter des erreurs
-	rotation_degrees = round(rotation_degrees / 90) * 90
 
 	# Corrige les sous-composants comme la caméra ou le stickman
-	adjust_stickman()
-	adjust_camera()
+	adjust_stickman(angle_degrees)
 
-func adjust_stickman():
+func adjust_stickman(angle_degrees):
 	# Modifie éventuellement l'orientation ou la logique de Stickman si nécessaire
-	var stickman = $StickmanObject
-	stickman.rotation_degrees = -rotation_degrees  # Corrige l'orientation du stickman (optionnel)
-
-func adjust_camera():
-	# Ajuste la rotation de la caméra pour garder une vue stable
-	var camera = $StickmanObject/Camera2D
-	camera.rotation_degrees = -rotation_degrees
+	var stickman = $Stickman
+	stickman.rotation_degrees -= angle_degrees # Corrige l'orientation du stickman (optionnel)
