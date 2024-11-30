6c54ba3d1bd89e8fbad00c83159557c23313dcdd
TR <tomriesepro@gmail.com>
2024-11-30 19:00:08 +0100

    ajout du level 3 avec la scie

---
+++ controleur/hacksaw.gd
@@ -0,0 +1,58 @@
+extends CharacterBody2D
+
+const SPEED = 30
+var direction = 1
+var sens = 1
+# Variable pour vérifier si le personnage est dans la hitbox
+var is_in_hitbox: bool = false
+
+func _physics_process(delta: float) -> void:
+	print(velocity)
+	if sens == 1:
+		velocity.y = 0
+		sensX()
+	else:
+		velocity.x = 0
+		sensY()
+	$Sprite.play("spin")
+	move_and_slide()
+	
+	if is_on_wall():
+		direction *= -1
+	
+	if is_on_floor():
+		direction *= -1
+	
+	if is_on_ceiling():
+		direction *= -1
+	
+func _process(delta: float) -> void:
+	# Vérifie si la touche "interact" est appuyée et si le personnage est dans la hitbox
+	if is_in_hitbox:
+		var path = get_tree().current_scene.scene_file_path
+		if path == "res://vue/tscn/level_3.tscn":
+			get_tree().change_scene_to_file("res://vue/tscn/level_3.tscn")
+	
+func sensX() -> void:
+	velocity.x = direction * SPEED
+	if direction == 1:
+		$Sprite.flip_h = false
+	else:
+		$Sprite.flip_h = true
+
+func sensY() -> void:
+	velocity.y = direction * SPEED
+	if direction == 1:
+		$Sprite.flip_v = false
+	else:
+		$Sprite.flip_v = true
+
+func _on_piques_body_entered(body: Node2D) -> void:
+	# Si le personnage entre dans la hitbox, active la variable
+	if body.name == "Stickman":
+		is_in_hitbox = true
+
+func _on_piques_body_exited(body: Node2D) -> void:
+	# Si le personnage sort de la hitbox, désactive la variable
+	if body.name == "Stickman":
+		is_in_hitbox = false
--- controleur/rotation.gd
+++ controleur/rotation.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 const ROTATION_ANGLE = 90  # Angle de rotation en degrés
 @onready var stickman = $Stickman
+@onready var hacksaw = $hacksaw
 
 func _input(event):
 	if not is_stickman_on_ground():
@@ -14,19 +15,25 @@ func _input(event):
 	elif Input.is_action_just_pressed("RotateRight"):
 		rotate_map(ROTATION_ANGLE)
 		$Rotation.play()
+	
 
 func rotate_map(angle_degrees):
 	# Applique la rotation de la carte
 	rotation_degrees += angle_degrees
-
 	# Ajuste le stickman pour compenser l'orientation
 	adjust_stickman(angle_degrees)
-	
+	if hacksaw != null:
+		adjust_hacksaw(angle_degrees)
 	await (0.5)
 
 func adjust_stickman(angle_degrees):
 	# Corrige l'orientation du stickman pour rester droit
 	stickman.rotation_degrees -= angle_degrees
+
+func adjust_hacksaw(angle_degrees):
+	# Corrige l'orientation de la scie pour rester droite
+	hacksaw.rotation_degrees -= angle_degrees
+	hacksaw.sens *= -1
 
 func is_stickman_on_ground() -> bool:
 	# Vérifie si le stickman est sur le sol (spécifique à CharacterBody2D)
