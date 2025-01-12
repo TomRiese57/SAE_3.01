40e6ebe016227d057f3f6206dd98055df40cfa92
TR <tomriesepro@gmail.com>
2025-01-12 14:17:31 +0100

    Modification pour acceuillir un classement

--- Godot/controleur/classement.gd
+++ Godot/controleur/classement.gd
@@ -1,4 +1,10 @@
 extends Control
 
+func _ready() -> void:
+	$VBoxContainer/Label.text = ""
+	var nb = 1
+	for x in Global.score:
+		$VBoxContainer/Label.text += "Score numéro " + str(nb) + " : temps : " + str(x[0]) + ", mort : " + str(x[1]) + "\n"
+
 func _on_retour_pressed() -> void:
 	get_tree().change_scene_to_file("res://vue/tscn/menu_principal.tscn")
--- Godot/controleur/win.gd
+++ Godot/controleur/win.gd
@@ -1,7 +1,10 @@
 extends Control
 
 func _ready() -> void:
-	$Result.text = "\nTemps : " + str(Global.time) + "\nMort : " + str(Global.dead)
+	$VBoxContainer/Result.text = "\nTemps : " + str(Global.time) + "\nMort : " + str(Global.dead)
+	Global.score.append([Global.time, Global.dead])
+	Global.time = 0
+	Global.dead = 0
 
 func _on_retour_pressed() -> void:
 	get_tree().change_scene_to_file("res://vue/tscn/menu_principal.tscn")
