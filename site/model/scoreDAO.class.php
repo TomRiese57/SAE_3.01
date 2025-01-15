319e3b3c483f8dfa4a9b17818b85840675a9ffd8
Loric <loric.bondon1@etu.univ-lorraine.fr>
2025-01-15 15:42:46 +0100

    modif scoreDAO

--- site/model/scoreDAO.class.php
+++ site/model/scoreDAO.class.php
@@ -2,66 +2,43 @@
 require_once 'connexion.php';
 require_once 'score.class.php';
 
-class ScoreDAO
-{
+class ScoreDAO {
     private $bd;
     private $select;
 
-    function __construct()
-    {
+    function __construct() {
         $this->bd = new Connexion();
         $this->select = 'SELECT id_score, id_uti, temps, morts, date FROM score';
     }
 
-    function insert(Score $score): void
-    {
+    function insert(Score $score): void {
         $this->bd->execSQL("INSERT INTO score (id_score, id_uti, temps, morts, date)
                             VALUES (:idScore, :idUti, :temps, :morts, :date)"
-            ,
-            [
-                ':idScore' => $score->getIdScore(),
-                ':idUti' => $score->getIdUti()
-                ,
-                ':temps' => $score->getTemps(),
-                ':morts' => $score->getMorts()
-                ,
-                ':date' => $score->getDate()
-            ]
-        );
+                            ,[':idScore'=>$score->getIdScore(), ':idUti'=>$score->getIdUti()
+                            ,':temps'=>$score->getTemps(), ':morts'=>$score->getMorts()
+                            ,':date'=>$score->getDate()]);
     }
 
-    function delete(int $idScore): void
-    {
+    function delete(int $idScore): void {
         $this->bd->execSQL("DELETE FROM score 
-                            WHERE id_score = :idScore", [':idScore' => $idScore]);
+                            WHERE id_score = :idScore", [':idScore'=>$idScore]);
     }
 
-    function deleteByIdUti(int $idUti): void
-    {
+    function deleteByIdUti(int $idUti): void {
         $this->bd->execSQL("DELETE FROM score 
-                            WHERE id_uti = :idUti", [':idUti' => $idUti]);
+                            WHERE id_uti = :idUti", [':idUti'=>$idUti]);
     }
 
-    function update(Score $score): void
-    {
+    function update(Score $score): void {
         $this->bd->execSQL("UPDATE score 
                             SET id_uti = :idUti, temps = :temps, morts = :morts, date = :date
                             WHERE id_score = :idScore"
-            ,
-            [
-                ':idScore' => $score->getIdScore(),
-                ':idUti' => $score->getIdUti()
-                ,
-                ':temps' => $score->getTemps(),
-                ':morts' => $score->getMorts()
-                ,
-                ':date' => $score->getDate()
-            ]
-        );
+                            ,[':idScore'=>$score->getIdScore(), ':idUti'=>$score->getIdUti()
+                            ,':temps'=>$score->getTemps(), ':morts'=>$score->getMorts()
+                            ,':date'=>$score->getDate()]);
     }
 
-    private function loadQuery(array $result): array
-    {
+    private function loadQuery(array $result): array {
         $scores = [];
         foreach ($result as $row) {
             $score = new Score();
@@ -75,23 +52,20 @@ class ScoreDAO
         return $scores;
     }
 
-    function getAll(): array
-    {
+    function getAll(): array {
         return ($this->loadQuery($this->bd->execSQLSelect($this->select)));
     }
 
-    function getAllAPI(): array
-    {
-        return $this->bd->execSQLselect("SELECT pseudo, temps, morts
-        FROM score, utilisateur
-        WHERE utilisateur.id_uti = score.id_uti");
+    function getAllAPI(): array {
+        return $this->bd->execSQLSelect("SELECT pseudo, temps, morts
+                                        FROM score, utilisateur
+                                        WHERE utilisateur.id_uti = score.id_uti");
     }
 
-    function getByIdScore(int $idScore): Score
-    {
+    function getByIdScore(int $idScore): Score {
         $unScore = new Score();
         $lesScores = $this->loadQuery($this->bd->execSQLSelect($this->select . " WHERE
-        id_score = :idScore", [':idScore' => $idScore]));
+        id_score = :idScore", [':idScore'=>$idScore]));
         if (count($lesScores) > 0) {
             $unScore = $lesScores[0];
         }
@@ -99,12 +73,11 @@ class ScoreDAO
         // il y a un seul élément dans le tableau des scores ➔ indice 0 return $unScore;
     }
 
-    function existe(int $idScore): bool
-    {
+    function existe(int $idScore): bool {
         $req = "SELECT * 
                 FROM score
                 WHERE id_score = :idScore";
-        $res = ($this->loadQuery($this->bd->execSQLSelect($req, [':idScore' => $idScore])));
+        $res = ($this->loadQuery($this->bd->execSQLSelect($req, [':idScore'=>$idScore])));
         return ($res != []); // si tableau des scores est vide alors le score n’existe pas
     }
 }
