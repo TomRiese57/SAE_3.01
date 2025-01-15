dcceb69bf16fdd0782f558b071fb7250addee91c
TR <tomriesepro@gmail.com>
2025-01-15 15:20:12 +0100

    nouveau web avec api pour Bondon

--- web/StickSpin.service.worker.js
+++ web/StickSpin.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1736932751|3016420208';
+const CACHE_VERSION = '1736950777|81203246';
 /** @type {string} */
 const CACHE_PREFIX = 'StickSpin-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
