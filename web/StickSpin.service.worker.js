7201756bab05e9a154aed1f449d688423ad4201e
TR <tomriesepro@gmail.com>
2025-01-16 15:29:53 +0100

    j'ai réussi à importé

--- web/StickSpin.service.worker.js
+++ web/StickSpin.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1737037541|377710905';
+const CACHE_VERSION = '1737037765|117032393';
 /** @type {string} */
 const CACHE_PREFIX = 'StickSpin-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
